77967c10c3762f7332c01690cc2d2e2af805eb83
Anastasiia <nastyak25555@gmail.com>
2024-06-18 13:50:34 +0200

    Create Drag Gesrure and Double Tap Gesture

--- Pinch/Screens/ContentView.swift
+++ Pinch/Screens/ContentView.swift
@@ -11,8 +11,16 @@ struct ContentView: View {
     // MARK: - PROPERTY
     @State private var isAnimated: Bool = false
     @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
     
     // MARK: - FUNCTION
+    
+    func resetImageState() {
+        return withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
     
     // MARK: - CONTENT
     
@@ -20,26 +28,41 @@ struct ContentView: View {
         NavigationView {
             ZStack {
                 // MARK: - PAGE IMAGE
-            Image("magazine-front-cover")
+                Image("magazine-front-cover")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .cornerRadius(10.0)
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
                     .opacity(isAnimated ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .scaleEffect(imageScale)
-                    // MARK: - 1. TAP GESTURE
+                
+                // MARK: - 1. TAP GESTURE
                     .onTapGesture(count: 2, perform: {
                         if imageScale == 1 {
                             withAnimation(.spring()) {
                                 imageScale = 5
                             }
                         } else {
-                            withAnimation(.spring()) {
-                                imageScale = 1
-                            }
+                            resetImageState()
                         }
-                    })
+                    }
+                )
+                // MARK: - 2. DRAG GESTURE
+                    .gesture(
+                        DragGesture()
+                            .onChanged { value in
+                                withAnimation(.linear(duration: 1)) {
+                                    imageOffset = value.translation
+                                }
+                            }
+                            .onEnded { _ in
+                                if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                            }
+                    )
             } //: ZStack
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
