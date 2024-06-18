d87da8eab4a8de9b8bdaabe7b003ac4450b58101
Anastasiia <nastyak25555@gmail.com>
2024-06-18 21:45:29 +0200

    Second commit

---
+++ Pinch/View/InfoPanelView.swift
@@ -0,0 +1,27 @@
+//
+//  InfoPanelView.swift
+//  Pinch
+//
+//  Created by Анастасия Кутняхова on 18.06.2024.
+//
+
+import SwiftUI
+
+struct InfoPanelView: View {
+    var body: some View {
+        HStack {
+            // MARK: - HOTSPOT
+            Image("")
+            
+            // MARK: - INFO PANEL
+            
+        }
+    }
+}
+
+#Preview {
+    InfoPanelView()
+        .preferredColorScheme(.dark)
+        .previewLayout(.sizeThatFits)
+        .padding()
+}
